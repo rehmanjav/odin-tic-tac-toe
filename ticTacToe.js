307ab5bd1ce22ce90dac6384f7639062cf8dc084
Rehman Javaid <rehmanjav@outlook.com>
2022-10-21 18:08:58 -0400

    Implement displayController

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,1 +1,35 @@
 "use strict"
+
+const displayController = (function() {  
+    // var _privateProperty = 'Hello World';
+    // var publicProperty = 'I am a public property';
+  
+    // function _privateMethod() {
+    //   console.log(_privateProperty);
+    // }
+
+    function _clearMain() {
+        let mainDiv = document.querySelector("main");
+
+        while(mainDiv.firstChild){
+            mainDiv.removeChild(mainDiv.firstChild);
+        }
+
+    }
+  
+    // function publicMethod() {
+    //   _privateMethod();
+    // }
+
+    function multiplayerNames() {
+
+    }
+  
+    return {
+    //   publicMethod: publicMethod,
+    //   publicProperty: publicProperty,
+      multiplayerNames: multiplayerNames,
+    };
+  })();
+  
+  
